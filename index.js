c0699f3d31828f15763d5fc4d9c3eae6066c3865
Rajdeep <rajdeepsarkarmr9126@gmail.com>
2025-07-27 01:48:39 +0530

    Added career page and updated contact page

--- index.js
+++ index.js
@@ -5,22 +5,16 @@ const port = 3000;
 const year = new Date().getFullYear();
 
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", { year });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about.ejs", { year });
-});
-
-app.get("/contact", (req, res) => {
-  res.render("contact.ejs", { year });
-});
-
-app.get("/testimonials", (req, res) => {
-  res.render("testimonials.ejs", { year });
-});
+// Routes
+app.get("/", (req, res) => res.render("index.ejs", { year }));
+app.get("/about", (req, res) => res.render("about.ejs", { year }));
+app.get("/contact", (req, res) => res.render("contact.ejs", { year }));
+app.get("/testimonials", (req, res) =>
+  res.render("testimonials.ejs", { year })
+);
+app.get("/career", (req, res) => res.render("career.ejs", { year }));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
